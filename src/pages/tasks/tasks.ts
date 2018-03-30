efd26d6cf496afb372fa4fa007333101712a9518
unknown <cedric>
2018-03-30 12:24:53 +0200

    added trips to calendar + removed useless lines of code

--- src/pages/tasks/tasks.ts
+++ src/pages/tasks/tasks.ts
@@ -3,7 +3,13 @@ import { IonicPage, NavController, NavParams,AlertController, ModalController } 
 import { TasksProvider } from '../../providers/tasks/tasks';
 import { TripsProvider } from '../../providers/trips/trips';
 import * as moment from 'moment';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/do';
 
+
+//import { forkJoin } from "rxjs/observable/forkJoin";
+import 'rxjs/add/observable/forkJoin';
 @IonicPage()
 @Component({
   selector: 'page-tasks',
@@ -27,15 +33,54 @@ export class TasksPage {
   public tasksProvider:TasksProvider,  public tripProvider: TripsProvider,
    private alertCtrl: AlertController) {
     this.toDoTasks=[];
-    //this.trips=[];
+    this.trips=[];
 
   }
 
 
-  // runs when the page become active and get the updated todoTasks
+  // runs every time the page become active
   ionViewWillEnter (){
-    this.getToDoTasks();
-    this.getTrips();
+  let tripsObservable = this.getTodoTasks(); // get the to-do tasks to be displayed
+  let tasksObservable = this.getTrips();     // get the trips
+
+
+Observable.forkJoin([tripsObservable,tasksObservable]).subscribe (data =>{
+  console.log("resultat ge:!", data)
+  let tasks:any=data[0];
+  for(let i =0;i<tasks.length;i++){
+    if(tasks[i].completed==false && tasks[i].deadline != null){
+      this.toDoTasks.push(tasks[i]);
+    }
+  }
+  this.formatTasksDeadlinesToUtc();
+
+  this.trips=data[1];
+  this.formatTripsDatesToUtc();
+  this.addToCalendar();
+
+});
+
+
+/*
+  Observable.forkJoin(
+
+        //  this.http.get('/app/books.json').map((res:Response) => res.json()),
+        //this.http.get('/app/movies.json').map((res:Response) => res.json())
+          this.getTodoTasks(),  // get the to-do tasks to be displayed
+          this.getTrips(),      // get the trips
+      ).subscribe(
+        data => {
+          let books = data[0]
+          let movies = data[1]
+        },
+        err => console.error(err)
+      );*/
+/*
+  Promise.all([
+    this.getTodoTasks(),  // get the to-do tasks to be displayed
+    this.getTrips(),      // get the trips
+  ]).then(value => this.addToCalendar());
+*/
 
   }
 
@@ -45,48 +90,63 @@ export class TasksPage {
     this.eventSource = [];
   }
 
+  getTodoTasks(){
+    console.log("getTodoTasks");
+    return this.tasksProvider.getAllRemoteTasks();
+  }
 
-  // only get the tasks that have not been checked yet
-  getToDoTasks(){
-    this.tasksProvider.getAllRemoteTasks().subscribe(data=>{
+  getTrips(){
+    console.log("getTrips");
+
+    return this.tripProvider.getRemoteTrips();
+  }
+  // get the tasks and trips
+  /*
+  getTodoTasks(){
+    return this.tasksProvider.getAllRemoteTasks().subscribe(data=>{
           let tasks:any=data;
           for(let i =0;i<tasks.length;i++){
             if(tasks[i].completed==false ){
               this.toDoTasks.push(tasks[i]);
             }
           }
-          this.addTasksToCalendar();
-
+          this.formatTasksDeadlinesToUtc();
+          console.log("on a les tasks !")
         });
   }
-
+*/
+/*
   getTrips(){
-    this.tripProvider.getRemoteTrips().subscribe(data=>{
+    return this.tripProvider.getRemoteTrips().subscribe(data=>{
             this.trips=data;
-            this.addTripsToCalendar();
+            this.formatTripsDatesToUtc();
+            console.log("on a les trips !")
         });
   }
+*/
+    // adds tasks and trip to calendar
+    addToCalendar() {
+      let events= this.eventSource; // all the previous events
+      for(let i =0;i<this.toDoTasks.length;i++){
 
 
-  //Calendar callbacks
+        events.push(this.toDoTasks[i]);
+      }
+      for(let i =0;i<this.trips.length;i++){
+        events.push(this.trips[i]);
+      }
+      console.log("this.eventSource",this.eventSource)
+      this.eventSource = [];
+      setTimeout(() => {
+        this.eventSource = events;
+        this.toDoTasks=[];
+      });
 
-  addTripsToCalendar(){
-    this.formatTripsDatesToUtc();
-    let events= this.eventSource; // all the previous entered tasks
-    for(let i =0;i<this.trips.length;i++){
-      events.push(this.trips[i]);
     }
-    console.log("event",this.eventSource)
-    this.eventSource = [];
-    setTimeout(() => {
-      this.eventSource = events;
-    });
-
-  }
 
   formatTripsDatesToUtc(){
     for(let i =0;i<this.trips.length;i++){
-
+/*
       let departure=this.trips[i].departure_date_time;
       let arrival=this.trips[i].arrival_date_time;
 
@@ -102,9 +162,9 @@ export class TasksPage {
       let arrivalHours  = new Date(departure).getHours();
       let arrivalMinutes= new Date(departure).getMinutes();
 
-      if(departureDay==arrivalDay && departureMonth==arrivalMonth && departureYear==arrivalYear){
-        arrivalDay+=1;
-      }
+      if(departureDay==arrivalDay   &&  departureMonth==arrivalMonth  &&
+         departureYear==arrivalYear &&  departureHours==arrivalHours  &&
+         departureMinutes==arrivalMinutes)  arrivalDay+=1;
 
       let tripData={   // data to be fed to the calendar
         allDay:false,
@@ -112,44 +172,50 @@ export class TasksPage {
         endTime:  new Date (Date.UTC( arrivalYear, arrivalMonth,arrivalDay,arrivalHours,arrivalMinutes)),
         title: "Trip: " + this.trips[i].departure_country + " to " + this.trips[i].arrival_country
       }
+
+      */
+
+
+      let tripData={   // data to be fed to the calendar
+        allDay:false,
+        startTime:new Date (this.trips[i].departure_date_time),
+        endTime:  new Date (this.trips[i].arrival_date_time),
+        title: "Trip: " + this.trips[i].departure_country + " to " + this.trips[i].arrival_country
+      }
+
+
       this.trips[i]=tripData;
     }
     console.log("voyage formate",this.trips)
 
   }
 
-  // adds tasks to calendar
-  addTasksToCalendar() {
-    this.formatTasksDeadlinesToUtc();
-    let events= this.eventSource; // all the previous entered tasks
-    for(let i =0;i<this.toDoTasks.length;i++){
-      events.push(this.toDoTasks[i]);
-    }
-    this.eventSource = [];
-    setTimeout(() => {
-      this.eventSource = events;
-      this.toDoTasks=[];
-    });
-
-  }
 
 // format the tasks'deadlines to be accepted by the calendar
 // see https://github.com/twinssbc/Ionic2-Calendar#eventsource
 formatTasksDeadlinesToUtc(){
 
   for(let i =0;i<this.toDoTasks.length;i++){
+
+    /*
     let year  = new Date(this.toDoTasks[i].deadline).getFullYear();
     let month = new Date(this.toDoTasks[i].deadline).getMonth();
     let day   = new Date(this.toDoTasks[i].deadline).getDate();
+    */
+    let taskTime:string=this.toDoTasks[i].deadline.split("-");
 
     let taskData={                                        // data to be fed to the calendar
       allDay:true,                                      // tasks only last one day
-      endTime:  new Date (Date.UTC( year, month,day+1)),
-      startTime:new Date (Date.UTC( year,month, day)),
+      //endTime:  new Date (Date.UTC( year, month,day+1)),
+      //startTime:new Date (Date.UTC( year,month, day)),
+      endTime:  new Date (+taskTime[0],+taskTime[1]-1,+taskTime[2]+1),
+      startTime:new Date (+taskTime[0],+taskTime[1]-1,+taskTime[2]),
       title: this.toDoTasks[i].title
     }
     this.toDoTasks[i]=taskData;
   }
+  console.log("tasks formate",this.toDoTasks)
+
 
 }
 
@@ -162,14 +228,26 @@ formatTasksDeadlinesToUtc(){
 
     onEventSelected(event) {
 
-      let start = moment(event.startTime).format('LL');
+      let start = moment(event.startTime).format('LLL');
+      let end = moment(event.endTime).format('LLL');
 
-      let alert = this.alertCtrl.create({
-        title: '' + event.title,
-        subTitle:"Deadline:<br>" + start,
-        buttons: ['OK']
-      })
-      alert.present();
+      if (event.title.substring(0,4)=="Trip"){
+        let alert = this.alertCtrl.create({
+          title: '' + event.title,
+          subTitle:"<br>" + "Departure:<br> " + start + "<br><br>Arrival:<br> "+ end ,
+          buttons: ['OK']
+        })
+        alert.present();
+      }
+      else{
+        let alert = this.alertCtrl.create({
+          title: '' + event.title,
+          subTitle:"<br>Deadline:<br>" + start,
+          buttons: ['OK']
+        })
+        alert.present();
+      }
+
 
     }
 
